141b53e8dbc26776912c46a3022e52a27be6946d
hexiyuanhecy <hexiyuanhecy@gmail.com>
2019-03-13 23:15:55 +0800

    现在是2019年3月13日，晚上11点14分，我在刷牙准备休息了。 最后把今天改的代码上传到GitHub，就这样。晚安。

---
+++ src/views/PagesView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pages-view">
+    <header-bar @showTalion="open"></header-bar>
+    <tab @showTalion="open"></tab>
+    <router-view></router-view>
+    <talion-view v-show="talion" @closeTalion="close"></talion-view>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '../components/HeaderBar'
+import Tab from '../components/Tab'
+import TalionView from '../views/TalionView'
+
+export default {
+  name: 'pages-view',
+  components: { HeaderBar, Tab, TalionView },
+  data () {
+    return {
+      talion: ''
+    }
+  },
+  methods: {
+    open: function () {
+      this.talion = 'open'
+    },
+    close: function () {
+      this.talion = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
